29f4c898cb792a6e94677923fc0243678f26e103
CaioNM <70524443+CaioNM@users.noreply.github.com>
2022-07-28 11:26:21 -0300

    Add files via upload

---
+++ Java Script Algorithms and Data Structure/46. Método Shift.js
@@ -0,0 +1,5 @@
+// Criando Variável
+const myArray = [["John", 23], ["dog", 3]];
+
+// Assim como o metodo "pop", o método "shift" remove do array, mas dessa vez, ao invez do último, o método "shift" remove o primeiro elemento
+const removedFromMyArray = myArray.shift();
