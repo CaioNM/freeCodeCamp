07e09eb17426217bcbe650a9f4890650c422df9d
CaioNM <70524443+CaioNM@users.noreply.github.com>
2022-07-28 10:07:37 -0300

    Dia 04
    Continuando com o curso de JS, mais 10% concluído. Começando conceitos de Array.
    Total Completo: Bloco 1 - 40%

---
+++ Java Script Algorithms and Data Structure/41. Index de Arrays.js
@@ -0,0 +1,4 @@
+// Index de array é organizado da mesma maneira que o index de outros formatos
+const myArray = [50, 60, 70];
+// Settando um valor do array a outra variavel
+const myData = myArray[0]
