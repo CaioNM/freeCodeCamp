2a59d489e9a9485ab2182d0a88e7324e88acbd61
CaioNM <70524443+CaioNM@users.noreply.github.com>
2022-07-29 10:56:33 -0300

    Dia 04.1
    Terminando estudo de arrays

---
+++ Java Script Algorithms and Data Structure/48. Criando Lista de Compras.js
@@ -0,0 +1,3 @@
+// Primeiro trabalhinho
+  // Criando uma lista de compra em um array multidimensional
+const myList = [["Chocolate", 15], ["Tomate", 16], ["Banana", 6], ["Pipoca", 2], ["Arroz", 5]];
