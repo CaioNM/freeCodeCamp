a92142bb793d7f4da94ade9ae476468eca9083ef
Caio Moura <70524443+CaioNM@users.noreply.github.com>
2022-08-03 14:40:35 -0300

    Dia 07.1
    Continuando com o curso de JS.
    - Terminando os Operadores Lógicos e Condicionais
    - Total Completo: Bloco 1 - 80%

---
+++ Java Script Algorithms and Data Structure/87. Atualizando Propriedades.js
@@ -0,0 +1,10 @@
+// Objeto criado
+const myDog = {
+  "name": "Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"]
+};
+
+// Alterendo uma propriedade de um objeto
+myDog.name = 'Happy Coder';
