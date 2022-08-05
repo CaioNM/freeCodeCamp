0afc35c22a9889e8b289d2645d6182176c4f8d43
Caio Moura <70524443+CaioNM@users.noreply.github.com>
2022-08-05 17:55:52 -0300

    Dia 09
    Continuando com o curso de JS.
    - Fim do Primeiro Bloco
    - Total Completo: 
    	- Basic JavaScript - 100%

---
+++ Java Script Algorithms and Data Structure/Basic JavaScript/106. Numeros Aleatórios.js
@@ -0,0 +1,7 @@
+function randomFraction() {
+  // Gerando numeros aleatorios,
+    // O numero sempre vai ser a partir de 0, mas nunca 1
+   return Math.random();
+}
+
+console.log(randomFraction())
