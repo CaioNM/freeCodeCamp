166dcf70e810cba5c776f490379c07eef260e457
Caio Moura <70524443+CaioNM@users.noreply.github.com>
2022-08-08 18:49:03 -0300

    Dia 10.1
    Continuando com o curso de JS.
    - Começo do Segundo Bloco
    - Total Completo: 
    	- Basic JavaScript - 100%
    	- ES6 - 45%

---
+++ Java Script Algorithms and Data Structure/ES6/11. Desestruturação em arrays compostos.js
@@ -0,0 +1,7 @@
+const LOCAL_FORECAST = {
+  yesterday: { low: 61, high: 75 },
+  today: { low: 64, high: 77 },
+  tomorrow: { low: 68, high: 80 }
+};
+
+const {today: {high: highToday, low:lowToday}} = LOCAL_FORECAST;
