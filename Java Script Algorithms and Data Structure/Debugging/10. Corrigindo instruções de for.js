5dc8e5ff3e1dbdce6cebe30d3690f91f6e3c51a0
Caio Moura <70524443+CaioNM@users.noreply.github.com>
2022-08-15 13:18:29 -0300

    Dia 15.1
    Continuando com o curso de JS.
    - Final do Quarto Bloco
    - Total Completo: 
    	- Basic JavaScript - 100%
    	- ES6 - 100%
    	- Regular Expressions - 100%
            - Debugging - 100%

---
+++ Java Script Algorithms and Data Structure/Debugging/10. Corrigindo instruções de for.js
@@ -0,0 +1,10 @@
+// Corrigindo instruções de for
+function countToFive() {
+  let firstFive = "12345";
+  let len = firstFive.length;
+  for (let i = 0; i < len; i++) {
+    console.log(firstFive[i]);
+  }
+}
+
+countToFive();
---
+++ Java Script Algorithms and Data Structure/Debugging/8. Chamando funções corretamente.js
@@ -0,0 +1,9 @@
+// Chamando funções corretamente
+function getNine() {
+  let x = 6;
+  let y = 3;
+  return x + y;
+}
+
+let result = getNine();
+console.log(result);
