29bb501c7344827f0dd7288e307e364c5cdc8196
Caio Moura <70524443+CaioNM@users.noreply.github.com>
2022-08-25 12:40:22 -0300

    Dia 20
    Continuando com o curso de JS.
    - Final do Sétimo Bloco (Programação Orientado ao Objeto)
    - Total Completo: 
    	- Basic JavaScript - 100%
    	- ES6 - 100%
    	- Regular Expressions - 100%
            - Debugging - 100%
    	- Basic Data Structures - 100%
            - Basic Algorithm Scripting - 100%
    	- Object Oriented Programming - 100%

---
+++ Java Script Algorithms and Data Structure/Object Oriented Programming/25. IIFE - Immediately Invoked Function Expression.js
@@ -0,0 +1,6 @@
+// IIFE - Immediately Invoked Function Expression
+  // expressão de função invocada imediatamente
+    // São funções que são executadas logo após serem criadas
+(function (){
+  console.log("A cozy nest is ready");
+})();
